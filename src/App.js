a13c08be15bea9788e5c07469a4e064512b33eb3
Alberto <alberto.gonzalez@unosquare.com>
2021-02-26 13:00:10 -0600

    The useReducer class was completed.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from "react";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "buttonClick":
+      return { ...state, count: state.count + 1 };
+    case "setUsername":
+      return { ...state, username: action.username };
+    default:
+      break;
+  }
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, { count: 0, username: "" });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => dispatch({ type: "buttonClick" })}>
+        Click me
+      </button>
+      Current count is {state.count}
+      <input
+        type="text"
+        value={state.username}
+        onChange={(e) =>
+          dispatch({ type: "setUsername", username: e.target.value })
+        }
+      ></input>
+      Your current user name is {state.username}
     </div>
   );
 }
